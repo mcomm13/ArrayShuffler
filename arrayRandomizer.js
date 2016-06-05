d1075b4e78641035e457bc0964de3cd4904d2252
Mike Commendatore <michael.commendatore@outlook.com>
2016-06-05 18:42:51 -0400

    Added comments

--- arrayRandomizer.js
+++ arrayRandomizer.js
@@ -1,11 +1,13 @@
-var originalArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var copyOfOriginalArray = originalArray.slice();
+var originalArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];  //Set the array of items that you'll be working with
+var copyOfOriginalArray = originalArray.slice();  //Create a copy of it by value 
 var randomizedArray = [];
 
 window.onload = function() {
-    DisplayArray(originalArray, 'originalArrayDiv');;
+    DisplayArray(originalArray, 'originalArrayDiv');  //Display the original array on the web page
 }
 
+//A function that displays a specified array on a web page
+//The web page must contain an HTML element with the ID of the specified 'outputId'
 function DisplayArray(array, outputId) {
     var output = '';
     for (var i=0; i <= array.length - 1; i++) {
@@ -20,20 +22,26 @@ function DisplayArray(array, outputId) {
     targetDiv.innerHTML = output;   
 }
 
+
+//A function that randomly shuffles the original array
 function RandomizeArray() {
     var randomIndex;
     var itemToAdd;
-    var originalArrayLength = originalArray.length;
+    var originalArrayLength = originalArray.length;  //Need to know the length of the original array in its initial form
     
+    //A loop that randomly choses an index of the remaining array,
+    //then selects the item in the remaining array at that index,
+    //then puts that item into the new "shuffled" array,
+    //then removes that item from the original array
     for (var i=0; i <= originalArrayLength - 1; i++) {
-        randomIndex = Math.floor((Math.random() * (originalArrayLength - i)));
-        itemToAdd = originalArray[randomIndex];
+        randomIndex = Math.floor((Math.random() * (originalArrayLength - i)));  //Select an index of the remaining original array
+        itemToAdd = originalArray[randomIndex];  //Select the item in the original array that is at the randomly chosen index
         
-        randomizedArray[i] = itemToAdd;
-        originalArray.splice(randomIndex, 1);  //Remove array element at index of 'randomIndex'
+        randomizedArray[i] = itemToAdd;         //Put that item in the new array
+        originalArray.splice(randomIndex, 1);  //Remove array element at the index of the randomly chosen index
     }
     
-    DisplayArray(randomizedArray, 'randomizedArrayDiv');
+    DisplayArray(randomizedArray, 'randomizedArrayDiv');  //Once the new shuffled array is completely built, display it on the web page
     
     originalArray = copyOfOriginalArray.slice(); //In case you want to re-sort it, you need to restore the originalArray back to its initial form
 }
